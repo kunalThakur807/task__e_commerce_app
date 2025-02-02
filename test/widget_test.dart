648e6ba27a3b50d95ff740c2ef74abecd23787ab
kunal-thakur123 <kunal@mykcs.in>
2025-02-03 03:52:39 +0530

    test changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,9 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:app_task/main.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-    await tester.pump();
-  });
-}
+void main() {}
