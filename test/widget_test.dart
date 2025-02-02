e6de0f3a57b305b12e3c676abd7fba763cea5d4c
kunal-thakur123 <kunal@mykcs.in>
2025-02-03 03:56:58 +0530

    test-cases

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,4 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:app_task/main.dart';
 
-void main() {}
+void main() {
+  test('dummy test', () {
+    expect(1, equals(1)); // Always passes ✅
+  });
+}
